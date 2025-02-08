d045c3abbf412acd90e0d3d4671bb19051e01b31
Kate-Gornostaeva <itsmekate153@gmail.com>
2025-02-08 21:17:33 +1000

    csv file with import csv

--- lightsparse/lightsparse/spiders/lightsnewparse.py
+++ lightsparse/lightsparse/spiders/lightsnewparse.py
@@ -1,23 +1,30 @@
 import scrapy
+import csv
 
 
 class LightsnewparseSpider(scrapy.Spider):
     name = "lightsnewparse"
-    allowed_domains = ["https://svetilnik-online.ru"]
+    allowed_domains = ["svetilnik-online.ru"]
     start_urls = ["https://svetilnik-online.ru/lustri"]
-
-    custom_settings = {
-        'FEED_URI': 'lights_data.csv',
-        'FEED_FORMAT': 'csv',
-        'FEED_EXPORT_FIELDS': ["Наименование", "Цена", "Ссылка"],
-        'FEED_OVERWRITE': True,
-    }
 
     def parse(self, response):
         lights = response.css('li.item')
+        data = []  # Создаем список для хранения данных
+
         for light in lights:
-            yield {
-                "Наименование": light.css('a').attrib['title'],
-                "Цена": light.css('span.pprice::text').get(),
-                "Ссылка": light.css('a').attrib['href'],
-            }
+            name = light.css('a').attrib['title']
+            price = light.css('span.pprice::text').get()
+            link = light.css('a').attrib['href']
+
+            data.append({'Наименование': name, 'Цена': price, 'Ссылка': link})  # Добавляем данные в список
+
+        # Записываем данные в CSV файл
+        filename = "lights_data.csv"
+        with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['Наименование', 'Цена', 'Ссылка']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()  # Записываем заголовки столбцов
+            writer.writerows(data)  # Записываем данные
+
+        
